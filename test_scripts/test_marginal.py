cae63b538fc246a7c8386b390e62eaa84b65a2cf
alex <argriffi@ncsu.edu>
2016-02-18 12:07:16 -0500

    more test coverage

--- test_scripts/test_marginal.py
+++ test_scripts/test_marginal.py
@@ -1,7 +1,5 @@
 """
-Test log likelihoods and derivatives of log likelihoods.
-
-It is only checked for internal consistency, not for accuracy.
+Test marginal distribution reductions.
 
 """
 from __future__ import print_function, division
@@ -17,6 +15,7 @@ from numpy.testing import (
 
 from arbplf import arbplf_ll
 from arbplf import arbplf_deriv
+from arbplf import arbplf_marginal
 
 
 # 7 edges
@@ -58,10 +57,9 @@ def _assert_allclose_series(a, b):
     u, v = a.align(b)
     assert_allclose(u.values, v.values)
 
-
 def myll(d):
     """
-    Provides a dict -> pandas.DataFrame wrapper of the pure JSON arbplf_ll.
+    Provides a dict -> pandas.DataFrame wrapper of the JSON arbplf_ll.
     """
     s = arbplf_ll(json.dumps(d))
     df = pd.read_json(StringIO(s), orient='split', precise_float=True)
@@ -75,59 +73,41 @@ def myderiv(d):
     df = pd.read_json(StringIO(s), orient='split', precise_float=True)
     return df
 
-def test_default_ll():
-    myll(default_in)
+def mymarginal(d):
+    """
+    Provides a dict -> pandas.DataFrame wrapper of the JSON arbplf_marginal.
+    """
+    s = arbplf_marginal(json.dumps(d))
+    df = pd.read_json(StringIO(s), orient='split', precise_float=True)
+    return df
 
-def test_default_deriv():
-    myderiv(default_in)
+def test_default_marginal():
+    mymarginal(default_in)
 
-def test_default_ll_vs_empty_reductions():
-    f = myll
+def test_default_marginal_vs_empty_reductions():
+    f = mymarginal
     x = copy.deepcopy(default_in)
     x['site_reduction'] = {}
-    u = f(default_in)
-    v = f(x)
-    assert_allclose(u.values, v.values)
-
-def test_default_deriv_vs_empty_reductions():
-    f = myderiv
-    x = copy.deepcopy(default_in)
-    x['site_reduction'] = {}
-    x['edge_reduction'] = {}
+    x['node_reduction'] = {}
+    x['state_reduction'] = {}
     v = f(default_in)
     u = f(x)
     assert_allclose(u.values, v.values)
 
 
-def test_deriv_sum_over_sites():
+def test_marginal_sum_over_sites():
+    # see comments in test_deriv_sum_over_sites for more detail
 
-    # Create a full pandas DataFrame with columns 'site', 'edge', and 'value'.
-    # The 'site' and 'edge' columns contain indices,
-    # and the 'value' column contains floating point derivatives of the
-    # phylogenetic log likelihood.
-    full = myderiv(default_in)
+    full = mymarginal(default_in)
 
-    # Reduce the table to a pandas Series, where entries are indexed
-    # by edge and whose values are floating point derivatives.
-    # Reduction is by summation.
-    #pd_sum = pd.pivot_table(full, 'value', columns='edge', aggfunc=np.sum)
+    # Average over all axes *except* 'node' and 'state'.
+    pd_sum = full.groupby(['node', 'state']).value.mean()
 
-    # Alternatively, create a pandas Series.
-    # The values are summed over all axes except 'edge'.
-    pd_sum = full.groupby('edge').value.sum()
-
-    # Recompute the phylogenetic log likelihood derivatives,
-    # but this time aggregating sites by summation, using internal
-    # mechanisms of the arbplf_deriv C function that make use of
-    # linearity of summation and of error bounds.
-    # The result is a pandas Series.
-    # This method is potentially more accurate and efficient.
-    # Edges are selected in a weird order for the purpose of illustrating
-    # layout differences.
+    # Use the more accurate and efficient built-in aggregation mechanism.
     x = copy.deepcopy(default_in)
-    x['site_reduction'] = {'aggregation' : 'sum'}
-    x['edge_reduction'] = {'selection' : [4, 5, 6, 3, 2, 1, 0]}
-    phyly_sum = myderiv(x).set_index('edge').value
+    x['site_reduction'] = {'aggregation' : 'avg'}
+    x['node_reduction'] = {'selection' : [4, 5, 6, 7, 3, 2, 1, 0]}
+    phyly_sum = mymarginal(x).set_index(['node', 'state']).value
 
     # These pandas Series objects should contain the same information,
     # up to small numerical errors, but the underlying arrays are not
@@ -136,6 +116,43 @@ def test_deriv_sum_over_sites():
 
     _assert_allclose_series(pd_sum, phyly_sum)
 
+
+def test_marginal_via_likelihood():
+    # A marginal state distribution at a node can be computed
+    # inefficiently using only a likelihood calculation.
+    # Check that this inefficient likelihood-only method gives the same
+    # result as the more efficient method.
+
+    # Pick an arbitrary site and an arbitrary node.
+    node = 6
+    site = 1
+
+    # Compute marginal probabilities for 4 states at the node and site.
+    x = copy.deepcopy(default_in)
+    x['node_reduction'] = {'selection' : [node], 'aggregation' : 'sum'}
+    x['site_reduction'] = {'selection' : [site], 'aggregation' : 'sum'}
+    distn_via_marginal = mymarginal(x).set_index('state').value
+
+    # Compute log likelihoods for 4 states at the node and site.
+    # Use the log likelihoods to define a distribution over the states.
+    y = copy.deepcopy(default_in)
+    arr = []
+    for i in range(4):
+        m = copy.deepcopy(
+                default_in['model_and_data']['probability_array'][site])
+        m[node] = [0, 0, 0, 0]
+        m[node][i] = 1
+        arr.append(m)
+    y['model_and_data']['probability_array'] = arr
+    series_ll = myll(y).set_index('site').value
+    lhoods = series_ll.apply(np.exp)
+    distn_via_likelihood = lhoods / lhoods.sum()
+
+    _assert_allclose_series(distn_via_marginal, distn_via_likelihood)
+
+
+
+"""
 
 class CheckDeriv(object):
 
@@ -147,7 +164,7 @@ class CheckDeriv(object):
         self.y = copy.deepcopy(default_in)
 
 
-class TestDerivAggregation(CheckDeriv, TestCase):
+class CheckDerivAggregation(CheckDeriv, TestCase):
 
     def __init__(self, *args, **kwargs):
         TestCase.__init__(self, *args, **kwargs)
@@ -157,25 +174,29 @@ class TestDerivAggregation(CheckDeriv, TestCase):
         f = self.f.groupby('edge').value.sum()
         self.x['site_reduction'] = {'aggregation' : 'sum'}
         g = myderiv(self.x).set_index('edge').value
-        _assert_allclose_series(f, g)
+        u, v = f.align(g)
+        assert_allclose(u.values, v.values)
 
     def test_sum_over_edges(self):
         f = self.f.groupby('site').value.sum()
         self.x['edge_reduction'] = {'aggregation' : 'sum'}
         g = myderiv(self.x).set_index('site').value
-        _assert_allclose_series(f, g)
+        u, v = f.align(g)
+        assert_allclose(u.values, v.values)
 
     def test_avg_over_sites(self):
         f = self.f.groupby('edge').value.mean()
         self.x['site_reduction'] = {'aggregation' : 'avg'}
         g = myderiv(self.x).set_index('edge').value
-        _assert_allclose_series(f, g)
+        u, v = f.align(g)
+        assert_allclose(u.values, v.values)
 
     def test_avg_over_edges(self):
         f = self.f.groupby('site').value.mean()
         self.x['edge_reduction'] = {'aggregation' : 'avg'}
         g = myderiv(self.x).set_index('site').value
-        _assert_allclose_series(f, g)
+        u, v = f.align(g)
+        assert_allclose(u.values, v.values)
 
     def test_edge_sum_site_avg(self):
         f = self.f.groupby('site').value.sum().mean()
@@ -209,30 +230,34 @@ class TestDerivAggregation(CheckDeriv, TestCase):
         f = self.f.groupby('site').value.sum()
         self.x['edge_reduction'] = {'aggregation' : [1, 1, 1, 1, 1, 1, 1]}
         g = myderiv(self.x).value
-        _assert_allclose_series(f, g)
+        u, v = f.align(g)
+        assert_allclose(u, v)
 
     def test_edge_weighted_sum_vs_avg(self):
         p = 1 / 7
         f = self.f.groupby('site').value.mean()
         self.x['edge_reduction'] = {'aggregation' : [p, p, p, p, p, p, p]}
         g = myderiv(self.x).value
-        _assert_allclose_series(f, g)
+        u, v = f.align(g)
+        assert_allclose(u, v)
 
     def test_site_weighted_sum_vs_sum(self):
         f = self.f.groupby('edge').value.sum()
         self.x['site_reduction'] = {'aggregation' : [1, 1]}
         g = myderiv(self.x).value
-        _assert_allclose_series(f, g)
+        u, v = f.align(g)
+        assert_allclose(u, v)
 
     def test_edge_weighted_sum_vs_avg(self):
         p = 1 / 2
         f = self.f.groupby('edge').value.mean()
         self.x['site_reduction'] = {'aggregation' : [p, p]}
         g = myderiv(self.x).value
-        _assert_allclose_series(f, g)
+        u, v = f.align(g)
+        assert_allclose(u, v)
 
 
-class TestDerivSelection(CheckDeriv, TestCase):
+class CheckDerivSelection(CheckDeriv, TestCase):
 
     def __init__(self, *args, **kwargs):
         TestCase.__init__(self, *args, **kwargs)
@@ -243,7 +268,8 @@ class TestDerivSelection(CheckDeriv, TestCase):
         f = f.set_index(['site', 'edge']).value
         self.x['edge_reduction'] = {'selection' : [2, 4, 3]}
         g = myderiv(self.x).set_index(['site', 'edge']).value
-        _assert_allclose_series(f, g)
+        u, v = f.align(g)
+        assert_allclose(u.values, v.values)
 
     def test_site_selection(self):
         # Use pandas to select a subset of the results
@@ -256,10 +282,12 @@ class TestDerivSelection(CheckDeriv, TestCase):
         self.x['site_reduction'] = {'selection' : [1]}
         g = myderiv(self.x).set_index(['site', 'edge']).value
 
-        _assert_allclose_series(f, g)
+        # Check that the results are equal.
+        u, v = f.align(g)
+        assert_allclose(u.values, v.values)
 
 
-class TestDerivSelectionAggregation(CheckDeriv, TestCase):
+class CheckDerivSelectionAggregation(CheckDeriv, TestCase):
 
     def __init__(self, *args, **kwargs):
         TestCase.__init__(self, *args, **kwargs)
@@ -273,7 +301,8 @@ class TestDerivSelectionAggregation(CheckDeriv, TestCase):
                 'aggregation' : [1, 2]}
         f = myderiv(self.x).set_index('edge').value
         g = myderiv(self.y).set_index('edge').value
-        _assert_allclose_series(f, g)
+        u, v = f.align(g)
+        assert_allclose(g, f)
 
     def test_edge_selection_aggregation_sum(self):
         self.x['edge_reduction'] = {
@@ -283,7 +312,8 @@ class TestDerivSelectionAggregation(CheckDeriv, TestCase):
                 'aggregation' : [0, 0, 1, 0, 1, 0, 1]}
         f = myderiv(self.x).set_index('site').value
         g = myderiv(self.y).set_index('site').value
-        _assert_allclose_series(f, g)
+        u, v = f.align(g)
+        assert_allclose(g, f)
 
     def test_edge_selection_aggregation_duplicate_sum(self):
         self.x['edge_reduction'] = {
@@ -293,7 +323,8 @@ class TestDerivSelectionAggregation(CheckDeriv, TestCase):
                 'aggregation' : [0, 0, 0, 0, 2, 0, 0]}
         f = myderiv(self.x).set_index('site').value
         g = myderiv(self.y).set_index('site').value
-        _assert_allclose_series(f, g)
+        u, v = f.align(g)
+        assert_allclose(g, f)
 
     def test_edge_selection_aggregation_cancelling_sum(self):
         self.x['edge_reduction'] = {
@@ -303,7 +334,8 @@ class TestDerivSelectionAggregation(CheckDeriv, TestCase):
                 'aggregation' : [0, 0, 0, 0, 0, 0, 0]}
         f = myderiv(self.x).set_index('site').value
         g = myderiv(self.y).set_index('site').value
-        _assert_allclose_series(f, g)
+        u, v = f.align(g)
+        assert_allclose(g, f)
 
     def test_edge_selection_aggregation_weighted_sum(self):
         self.x['edge_reduction'] = {
@@ -313,4 +345,7 @@ class TestDerivSelectionAggregation(CheckDeriv, TestCase):
                 'aggregation' : [0, 0, 3.14, 0, 5, 0, 42]}
         f = myderiv(self.x).set_index('site').value
         g = myderiv(self.y).set_index('site').value
-        _assert_allclose_series(f, g)
+        u, v = f.align(g)
+        assert_allclose(g, f)
+
+"""
