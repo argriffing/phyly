90c2775a5f125775202687d65dc5622e551d34aa
alex <argriffi@ncsu.edu>
2016-05-10 18:11:42 -0400

    remove redundant code

--- src/arbplfdwell.c
+++ src/arbplfdwell.c
@@ -368,30 +368,18 @@ static void
 _nd_accum_update(nd_accum_t arr,
         likelihood_ws_t w, cross_site_ws_t csw, model_and_data_t m, slong prec)
 {
-    int state, site, edge, idx, cat;
-    arb_t site_lhood, cat_lhood, prior_prob, cat_rate, rate, lhood;
-    nd_axis_struct *state_axis, *site_axis, *edge_axis;
+    int state, site, idx, cat;
+    nd_axis_struct *state_axis, *site_axis;
     int *coords;
 
-    slong node_count = model_and_data_node_count(m);
     slong edge_count = model_and_data_edge_count(m);
     slong state_count = model_and_data_state_count(m);
     slong site_count = model_and_data_site_count(m);
     slong rate_category_count = model_and_data_rate_category_count(m);
 
-    arb_init(site_lhood);
-    arb_init(cat_lhood);
-    arb_init(prior_prob);
-    arb_init(cat_rate);
-    arb_init(rate);
-    arb_init(lhood);
-
     coords = malloc(arr->ndim * sizeof(int));
 
-    arb_init(lhood);
-
     site_axis = arr->axes + SITE_AXIS;
-    edge_axis = arr->axes + EDGE_AXIS;
     state_axis = arr->axes + STATE_AXIS;
 
     /* zero all requested cells of the array */
@@ -416,6 +404,9 @@ _nd_accum_update(nd_accum_t arr,
          */
         {
             arb_mat_t P, L, Q;
+            arb_t cat_rate, rate;
+            arb_init(cat_rate);
+            arb_init(rate);
             arb_mat_init(P, state_count, state_count);
             arb_mat_init(L, state_count, state_count);
             arb_mat_init(Q, state_count, state_count);
@@ -433,6 +424,8 @@ _nd_accum_update(nd_accum_t arr,
                     _arb_mat_exp_frechet(P, fmat, Q, L, prec);
                 }
             }
+            arb_clear(cat_rate);
+            arb_clear(rate);
             arb_mat_clear(P);
             arb_mat_clear(L);
             arb_mat_clear(Q);
@@ -444,106 +437,9 @@ _nd_accum_update(nd_accum_t arr,
             if (!site_axis->request_update[site]) continue;
             coords[SITE_AXIS] = site;
 
-            /* update base node vectors */
-            pmat_update_base_node_vectors(
-                    w->base_node_vectors, m->p, site,
-                    m->root_prior, csw->equilibrium,
-                    m->preorder[0], prec);
-
-            /* clear cross-category expectations and site lhood */
-            _arb_vec_zero(w->cc_edge_expectations, edge_count);
-            arb_zero(site_lhood);
-
-            for (cat = 0; cat < rate_category_count; cat++)
-            {
-                arb_mat_struct *tmat_base, *fmat_base;
-                tmat_base = cross_site_ws_transition_matrix(csw, cat, 0);
-                fmat_base = cross_site_ws_dwell_frechet_matrix(csw, cat, 0);
-
-                /*
-                 * Update per-node and per-edge likelihood vectors.
-                 * Actually the likelihood vectors on edges are not used.
-                 * This is a backward pass from the leaves to the root.
-                 */
-                evaluate_site_lhood(lhood,
-                        w->lhood_node_vectors,
-                        w->lhood_edge_vectors,
-                        w->base_node_vectors,
-                        tmat_base,
-                        m->g, m->preorder, node_count, prec);
-
-                /*
-                 * Update marginal distribution vectors at nodes.
-                 * This is a forward pass from the root to the leaves.
-                 */
-                evaluate_site_marginal(
-                        w->marginal_node_vectors,
-                        w->lhood_node_vectors,
-                        w->lhood_edge_vectors,
-                        tmat_base,
-                        m->g, m->preorder, node_count, state_count, prec);
-
-                /* Update expectations at edges. */
-                evaluate_edge_expectations(
-                        w->edge_expectations,
-                        w->marginal_node_vectors,
-                        w->lhood_node_vectors,
-                        w->lhood_edge_vectors,
-                        fmat_base,
-                        m->g, m->preorder, node_count, state_count, prec);
-
-                /* compute category likelihood */
-                rate_mixture_get_prob(prior_prob, m->rate_mixture, cat, prec);
-                arb_mul(cat_lhood, lhood, prior_prob, prec);
-                arb_add(site_lhood, site_lhood, cat_lhood, prec);
-
-                /* Accumulate cross-category expectations. */
-                for (edge = 0; edge < edge_count; edge++)
-                {
-                    if (!edge_axis->request_update[edge]) continue;
-                    idx = m->edge_map->order[edge];
-                    arb_addmul(w->cc_edge_expectations + idx,
-                               w->edge_expectations + idx, cat_lhood, prec);
-                }
-            }
-
-            /* Divide cross-category expectations by site lhood */
-            for (edge = 0; edge < edge_count; edge++)
-            {
-                if (!edge_axis->request_update[edge]) continue;
-                idx = m->edge_map->order[edge];
-                arb_div(w->cc_edge_expectations + idx,
-                        w->cc_edge_expectations + idx,
-                        site_lhood, prec);
-            }
-
-            /* Update the nd accumulator. */
-            for (edge = 0; edge < edge_count; edge++)
-            {
-                /* skip edges that are not requested */
-                if (!edge_axis->request_update[edge]) continue;
-                coords[EDGE_AXIS] = edge;
-
-                /*
-                 * Accumulate.
-                 * Note that the axes, accumulator, and json interface
-                 * work with "user" edge indices,
-                 * whereas the workspace arrays work with
-                 * tree graph preorder edge indices.
-                 */
-                idx = m->edge_map->order[edge];
-                nd_accum_accumulate(arr,
-                        coords, w->cc_edge_expectations + idx, prec);
-            }
+            _update_site(arr, w, csw, m, coords, site, prec);
         }
     }
-
-    arb_clear(site_lhood);
-    arb_clear(cat_lhood);
-    arb_clear(prior_prob);
-    arb_clear(cat_rate);
-    arb_clear(rate);
-    arb_clear(lhood);
 
     free(coords);
 }
