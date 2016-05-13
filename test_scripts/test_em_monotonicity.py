8edb9d306afbd4a172ac4ca5d69c7900c6a08715
alex <argriffi@ncsu.edu>
2016-05-12 21:04:29 -0400

    test EM with a rate mixture

--- test_scripts/test_em_monotonicity.py
+++ test_scripts/test_em_monotonicity.py
@@ -19,6 +19,21 @@ def _df(f, d):
     df = pd.read_json(StringIO(s), orient='split', precise_float=True)
     return df
 
+def sample_rate_matrix(n):
+    # Sample nonnegative off-diagonal entries of a rate matrix.
+    scale = np.random.exponential()
+    rate_matrix = scale * np.exp(np.random.randn(n, n))
+    np.fill_diagonal(rate_matrix, 0)
+    return rate_matrix
+
+def sample_rate_mixture():
+    # Sample a rate mixture.
+    ncats = np.random.randint(1, 5)
+    mixture_rates = np.exp(np.random.randn(ncats))
+    mixture_prior = np.exp(np.random.randn(ncats))
+    mixture_prior = mixture_prior / np.sum(mixture_prior)
+    return dict(rates=mixture_rates.tolist(), prior=mixture_prior.tolist())
+
 def check_em_monotonicity():
     # The tree is fixed with four nodes and three branches.
     edges = [[0, 1], [0, 2], [0, 3]]
@@ -29,9 +44,7 @@ def check_em_monotonicity():
     n = np.random.randint(2, 5)
 
     # Sample nonnegative off-diagonal entries of a rate matrix.
-    scale = np.random.exponential()
-    rate_matrix = scale * np.exp(np.random.randn(n, n))
-    np.fill_diagonal(rate_matrix, 0)
+    rate_matrix = sample_rate_matrix(n)
 
     # Sample nonnegative edge rate scaling factors.
     edge_rates_in = np.exp(np.random.randn(edge_count))
@@ -47,6 +60,11 @@ def check_em_monotonicity():
     m['edge_rate_coefficients'] = edge_rates_in.tolist()
     m['rate_matrix'] = rate_matrix.tolist()
     m['probability_array'] = arr.tolist()
+
+    # Maybe use a mixture of rates.
+    rate_mixture = sample_rate_mixture()
+    if np.random.randint(2):
+        m['rate_mixture'] = rate_mixture
 
     # ask for the log likelihood
     a_in = {'model_and_data' : m, 'site_reduction' : {"aggregation" : "sum"}}
