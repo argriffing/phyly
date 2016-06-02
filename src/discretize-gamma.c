b41ac3e3070761bd226187be2c7dc5a098812de5
alex <argriffi@ncsu.edu>
2016-06-02 14:43:53 -0400

    gamma discretization tests

--- src/discretize-gamma.c
+++ src/discretize-gamma.c
@@ -2,300 +2,31 @@
 #include "arb.h"
 #include "arb_calc.h"
 
-/*
- * TODO: These parameters should be provided on the commmand line
- *       instead of being hardcoded.
- *
- * According to R, one of the intermediate values should be:
- * > qgamma(0.25, shape=shape)
- * [1] 0.00048270865921202978522
- *
- */
-/* #define SHAPE 0.19242344607262146 */
-#define SHAPE 0.5
-#define NCATS 4
-
-
-static void
-_arb_hypgeom_gamma_lower(arb_t res, const arb_t s, const arb_t x,
-        int regularized, slong prec)
-{
-    if (regularized == 1 && arf_is_pos_inf(arb_midref(x)))
-    {
-        arb_one(res);
-    }
-    else
-    {
-        acb_t resc, sc, xc;
-        acb_init(resc);
-        acb_init(sc);
-        acb_init(xc);
-        arb_set(acb_realref(sc), s);
-        arb_set(acb_realref(xc), x);
-
-        //flint_printf("acb_hypgeom_gamma_lower input:\n");
-        //acb_printd(xc, 15); flint_printf("\n");
-
-        acb_hypgeom_gamma_lower(resc, sc, xc, regularized, prec);
-
-        //flint_printf("acb_hypgeom_gamma_lower output:\n");
-        //acb_printd(resc, 15); flint_printf("\n");
-
-        arb_set(res, acb_realref(resc));
-
-        acb_clear(resc);
-        acb_clear(sc);
-        acb_clear(xc);
-    }
-}
-
-typedef struct
-{
-    arb_t s; /* gamma shape parameter */
-    arb_t c; /* cdf value at quantile of interest */
-} quantile_args_struct;
-typedef quantile_args_struct quantile_args_t[1];
-
-static void
-quantile_args_init(quantile_args_t p)
-{
-    arb_init(p->s);
-    arb_init(p->c);
-}
-
-static void
-quantile_args_clear(quantile_args_t p)
-{
-    arb_clear(p->s);
-    arb_clear(p->c);
-}
-
-
-/* provides the arb_calc_func_t interface */
-static int
-_arb_gamma_objective(arb_t out, const arb_t inp,
-        void *param, slong order, slong prec)
-{
-    int regularized = 1;
-    const quantile_args_struct *p = param;
-
-    // flint_printf("x in:\n");
-    // arb_printd(inp, 15); flint_printf("\n");
-
-    // flint_printf("s in:\n");
-    // arb_printd(p->s, 15); flint_printf("\n");
-
-    _arb_hypgeom_gamma_lower(out, p->s, inp, regularized, prec);
-    // flint_printf("out before subtraction:\n");
-    // arb_printd(out, 15); flint_printf("\n");
-
-    arb_sub(out, out, p->c, prec);
-    // flint_printf("out after subtraction:\n");
-    // arb_printd(out, 15); flint_printf("\n");
-
-    // flint_printf("\n");
-
-    return 0;
-}
-
-int
-_sign(const arf_t q, const quantile_args_t p, slong prec)
-{
-    int sign;
-    int order = 1;
-    int regularized = 1;
-    int result;
-    arb_t x, y, g;
-
-    arb_init(x);
-    arb_init(y);
-    arb_init(g);
-
-    arb_set_arf(x, q);
-    /* for arb_calc callback compatibility we need to remove const */
-    result = _arb_gamma_objective(y, x, (quantile_args_struct *) p, 1, prec);
-
-    arb_sgn(g, y);
-    if (arb_contains_zero(g))
-    {
-        sign = 0;
-    }
-    else
-    {
-        sign = arf_sgn(arb_midref(g));
-    }
-
-    arb_clear(x);
-    arb_clear(y);
-    arb_clear(g);
-
-    return sign;
-}
-
-/* return an ARB_CALC_ code */
-static int
-_find_interval_bound(arf_interval_t r, int target_sign,
-        const quantile_args_t p, slong prec)
-{
-    int result = ARB_CALC_SUCCESS;
-    int sign;
-    arf_t q;
-    arf_struct *head, *tail;
-    if (target_sign == 1)
-    {
-        head = &(r->b);
-        tail = &(r->a);
-    }
-    else
-    {
-        head = &(r->a);
-        tail = &(r->b);
-    }
-    arf_init(q);
-    arf_one(q);
-    while (1)
-    {
-        sign = _sign(q, p, prec);
-        //flint_printf("sign=%d target=%d prec=%wd\n", sign, target_sign, prec);
-        if (sign == 0)
-        {
-            /* the sign is ambiguous */
-            result = ARB_CALC_IMPRECISE_INPUT;
-            break;
-        }
-        else if (sign == target_sign)
-        {
-            /* found the target sign */
-            result = ARB_CALC_SUCCESS;
-            arf_set(head, q);
-            break;
-        }
-        else
-        {
-            /* keep searching, towards zero or towards infinity */
-            arf_set(tail, q);
-            arf_mul_2exp_si(q, q, target_sign);
-        }
-    }
-    arf_clear(q);
-    return result;
-}
-
-
-int
-isolate_positive_quantile(arf_interval_t r,
-        const quantile_args_t p, slong prec)
-{
-    int result;
-
-    arf_nan(&r->a);
-    arf_nan(&r->b);
-
-    /* find the lower bound of the interval */
-    if (arf_is_nan(&r->a))
-    {
-        result = _find_interval_bound(r, -1, p, prec);
-        if (result != ARB_CALC_SUCCESS) return result;
-    }
-
-    /* find the upper bound of the interval */
-    if (arf_is_nan(&r->b))
-    {
-        result = _find_interval_bound(r, 1, p, prec);
-        if (result != ARB_CALC_SUCCESS) return result;
-    }
-
-    return result;
-}
-
-int
-compute_quantile(arb_t res, int k, slong prec)
-{
-    int result;
-    quantile_args_t p;
-    arf_interval_t r, refined;
-    slong maxiter;
-
-    quantile_args_init(p);
-    arf_interval_init(r);
-    arf_interval_init(refined);
-
-    arb_set_d(p->s, SHAPE);
-    arb_set_si(p->c, k);
-    arb_div_si(p->c, p->c, NCATS, prec);
-
-    result = isolate_positive_quantile(r, p, prec);
-    if (result != ARB_CALC_SUCCESS)
-    {
-        arb_indeterminate(res);
-        goto finish;
-    }
-
-    maxiter = 1000;
-    result = arb_calc_refine_root_bisect(
-            refined, _arb_gamma_objective, p, r, maxiter, prec);
-    arf_interval_get_arb(res, refined, prec);
-
-finish:
-
-    quantile_args_clear(p);
-    arf_interval_clear(r);
-    arf_interval_clear(refined);
-
-    return result;
-}
-
-int
-compute_expectile(arb_t expectile, const arb_t quantile, slong prec)
-{
-    int result;
-    int regularized = 1;
-    arb_t sp1;
-
-    arb_init(sp1);
-    arb_set_d(sp1, SHAPE);
-    arb_add_si(sp1, sp1, 1, prec);
-
-    _arb_hypgeom_gamma_lower(expectile, sp1, quantile, regularized, prec);
-
-    arb_clear(sp1);
-
-    return result;
-}
+#include "arb_vec_extras.h"
+#include "gamma_discretization.h"
 
 int
 main()
 {
-    int k;
-    arb_struct *quantiles, *expectiles, *rates;
+    slong k;
+    slong n = 4;
+    double shape = 0.5;
+    //double shape = 0.19242344607262146239;
+    arb_t s;
+    arb_struct *rates;
     slong prec = 4;
 
-    quantiles = _arb_vec_init(NCATS+1);
-    expectiles = _arb_vec_init(NCATS+1);
-    rates = _arb_vec_init(NCATS);
-    arb_zero(quantiles + 0);
-    arb_pos_inf(quantiles + NCATS);
+    arb_init(s);
+    arb_set_d(s, shape);
+    rates = _arb_vec_init(n);
 
     while (1)
     {
         slong worstprec;
-        for (k = 1; k < NCATS; k++)
+        gamma_rates(rates, n, s, prec);
+        for (k = 0; k < n; k++)
         {
-            compute_quantile(quantiles + k, k, prec);
-        }
-        for (k = 0; k < NCATS+1; k++)
-        {
-            compute_expectile(expectiles + k, quantiles + k, prec);
-            //arb_printd(expectiles + k, 15); flint_printf("\n");
-        }
-        //flint_printf("\n");
-
-        for (k = 0; k < NCATS; k++)
-        {
-            slong p;
-            arb_sub(rates + k, expectiles + k + 1, expectiles + k, prec);
-            arb_mul_si(rates + k, rates + k, NCATS, prec);
-            p = arb_rel_accuracy_bits(rates + k);
+            slong p = arb_rel_accuracy_bits(rates + k);
             if ((k == 0) || p < worstprec)
                 worstprec = p;
         }
@@ -304,30 +35,14 @@ main()
         prec <<= 1;
     }
 
-    flint_printf("quantiles:\n");
-    for (k = 0; k < NCATS+1; k++)
-    {
-        arb_printd(quantiles + k, 15);
-        flint_printf("\n");
-    }
-
-    flint_printf("expectiles:\n");
-    for (k = 0; k < NCATS+1; k++)
-    {
-        arb_printd(expectiles + k, 15);
-        flint_printf("\n");
-    }
-
     flint_printf("rates:\n");
-    for (k = 0; k < NCATS; k++)
+    for (k = 0; k < n; k++)
     {
         arb_printd(rates + k, 15);
         flint_printf("\n");
     }
 
-    _arb_vec_clear(quantiles, NCATS+1);
-    _arb_vec_clear(expectiles, NCATS+1);
-    _arb_vec_clear(rates, NCATS);
+    _arb_vec_clear(rates, n);
 
     return 0;
 }
---
+++ test_scripts/test_gamma_discretization.py
@@ -0,0 +1,119 @@
+"""
+Uses test_rate_mixture_vs_block.py as a template.
+
+See Figure 1 of
+"Maximum Likelihood Phylogenetic Estimation from DNA Sequences
+with Variable Rates over Sites: Approximate Methods"
+Ziheng Yang (1994) J Mol Evol.
+http://abacus.gene.ucl.ac.uk/ziheng/pdf/1994YangJMEv39p306.pdf
+
+"""
+from __future__ import print_function, division
+
+from StringIO import StringIO
+from functools import partial
+import json
+import copy
+import sys
+
+import numpy as np
+import pandas as pd
+from scipy.optimize import minimize
+from numpy.testing import (
+        assert_, assert_equal, assert_raises, assert_allclose, TestCase)
+
+from arbplf import (
+        arbplf_ll, arbplf_marginal,
+        arbplf_dwell, arbplf_trans, arbplf_em_update,
+        arbplf_newton_delta, arbplf_newton_update,
+        arbplf_newton_refine, arbplf_deriv, arbplf_hess,
+        arbplf_inv_hess,
+        )
+
+# These are easy to test, so they are tested in this module.
+# For now I'm skipping the newton refinement.
+_easy_funcs = (
+        arbplf_ll,
+        arbplf_deriv,
+        arbplf_hess, arbplf_inv_hess,
+        arbplf_newton_delta, arbplf_newton_update,
+        arbplf_em_update)
+
+# These are still easy to test, but are a bit more difficult.
+# They are not tested in this module.
+_less_easy_funcs = (
+        arbplf_marginal, arbplf_dwell, arbplf_trans, arbplf_newton_refine)
+
+def _df(f, d):
+    s_in = json.dumps(d)
+    s_out = f(s_in)
+    df = pd.read_json(StringIO(s_out), orient='split', precise_float=True)
+    return df
+
+def _assert_allclose_series(a, b):
+    # Align the layouts of the two pandas Series objects,
+    # preparing to compare their values.
+    # The align member function returns two new pandas Series objects.
+    u, v = a.align(b)
+    assert_allclose(u.values, v.values)
+
+
+# 7 edges
+# 2 sites
+default_in = {
+ "model_and_data" : {
+     "edges" : [[5, 0], [5, 1], [5, 6], [6, 2], [6, 7], [7, 3], [7, 4]],
+     "edge_rate_coefficients" : [0.01, 0.2, 0.15, 0.3, 0.05, 0.3, 0.02],
+     "root_prior" : "uniform_distribution",
+     "rate_matrix" : [
+         [0, .3, .4, .5],
+         [.3, 0, .3, .3],
+         [.3, .6, 0, .3],
+         [.3, .3, .3, 0]],
+     "probability_array" : [
+         [
+         [1, 0, 0, 0],
+         [0, 1, 0, 0],
+         [0, 1, 0, 0],
+         [0, 1, 0, 0],
+         [0, 0, 1, 0],
+         [1, 1, 1, 1],
+         [1, 1, 1, 1],
+         [1, 1, 1, 1]],
+         [
+         [1, 0, 0, 0],
+         [0, 1, 0, 0],
+         [0, 0, 0, 1],
+         [0, 1, 0, 0],
+         [0, 0, 1, 0],
+         [1, 1, 1, 1],
+         [1, 1, 1, 1],
+         [1, 1, 1, 1]]]},
+ "site_reduction" : {"aggregation" : "sum"},
+}
+
+def test_rate_mixture_equivalence():
+    # Check the gamma discretization.
+    gamma_rate_mixture = dict(
+            gamma_shape = 0.5,
+            gamma_categories = 4)
+    rate_mixture = dict(
+            rates = [
+                0.0333877533835995,
+                0.251915917593438,
+                0.820268481973649,
+                2.89442784704931],
+            prior = 'uniform_distribution')
+    #
+    for f in _easy_funcs:
+        print(f.__name__)
+        # let the engine do the discretization
+        x = copy.deepcopy(default_in)
+        x['model_and_data']['gamma_rate_mixture'] = gamma_rate_mixture
+        u = _df(f, x)
+        # use a precomputed discretization
+        x = copy.deepcopy(default_in)
+        x['model_and_data']['rate_mixture'] = rate_mixture
+        v = _df(f, x)
+        # check that the two analyses give the same results
+        assert_allclose(u.values, v.values)
