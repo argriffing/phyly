9f18c10b70a969a52d2b4a2d3b3c9ea97a903a67
alex <argriffi@ncsu.edu>
2016-06-07 15:14:06 -0400

    speed up gamma discretization when the gamma shape parameter is tiny

--- src/gamma_discretization.c
+++ src/gamma_discretization.c
@@ -54,9 +54,45 @@ _arb_hypgeom_gamma_lower(arb_t res, const arb_t s, const arb_t x,
     }
 }
 
+
 /* provides the arb_calc_func_t interface */
 static int
-_arb_gamma_objective(arb_t out, const arb_t inp,
+_arb_gamma_objective(arb_ptr out, const arb_t inp,
+        void *param, slong order, slong prec)
+{
+    acb_t s;
+    acb_ptr z, y;
+    int regularized = 1;
+    const quantile_args_struct *p = param;
+    int zlen = FLINT_MIN(2, order);
+
+    acb_init(s);
+    z = _acb_vec_init(zlen);
+    y = _acb_vec_init(order);
+
+    arb_set(acb_realref(s), p->s);
+    arb_set(acb_realref(z + 0), inp);
+    if (zlen > 1)
+        acb_one(z + 1);
+
+    _acb_hypgeom_gamma_lower_series(y, s, z, zlen, regularized, order, prec);
+
+    {
+        slong i;
+        for (i = 0; i < order; i++)
+            arb_set(out + i, acb_realref(y + i));
+    }
+
+    arb_sub(out, out, p->c, prec);
+
+    return 0;
+}
+
+
+/* fixme: deprecated */
+/* provides the arb_calc_func_t interface */
+static int
+old_arb_gamma_objective(arb_t out, const arb_t inp,
         void *param, slong order, slong prec)
 {
     int regularized = 1;
@@ -99,8 +135,8 @@ _sign(const arf_t q, const quantile_args_t p, slong prec)
 
 /* return an ARB_CALC_ code */
 static int
-_find_interval_bound(arf_interval_t r, int target_sign,
-        const quantile_args_t p, slong prec)
+_find_interval_bound(arf_interval_t r, const arf_t initial,
+        int target_sign, const quantile_args_t p, slong prec)
 {
     int result = ARB_CALC_SUCCESS;
     int sign;
@@ -118,7 +154,7 @@ _find_interval_bound(arf_interval_t r, int target_sign,
         tail = &(r->b);
     }
     arf_init(q);
-    arf_one(q);
+    arf_set(q, initial);
     logratio = target_sign;
     while (1)
     {
@@ -155,7 +191,7 @@ _find_interval_bound(arf_interval_t r, int target_sign,
 
 
 static int
-isolate_positive_quantile(arf_interval_t r,
+isolate_positive_quantile(arf_interval_t r, const arf_t initial,
         const quantile_args_t p, slong prec)
 {
     int result;
@@ -166,17 +202,173 @@ isolate_positive_quantile(arf_interval_t r,
     /* find the lower bound of the interval */
     if (arf_is_nan(&r->a))
     {
-        result = _find_interval_bound(r, -1, p, prec);
+        result = _find_interval_bound(r, initial, -1, p, prec);
         if (result != ARB_CALC_SUCCESS) return result;
     }
 
     /* find the upper bound of the interval */
     if (arf_is_nan(&r->b))
     {
-        result = _find_interval_bound(r, 1, p, prec);
+        result = _find_interval_bound(r, initial, 1, p, prec);
         if (result != ARB_CALC_SUCCESS) return result;
     }
 
+    return result;
+}
+
+/* a helper function for gamma quantiles */
+static void
+_temme_r(arb_t r, const arb_t a, const arb_t p, slong prec)
+{
+    /* (p*gamma(1+a))^(1/a) = exp((log(p) + lgamma(1+a))/a) */
+    arb_t logp;
+    arb_init(logp);
+
+    if (r == p || r == a) abort(); /* assert */
+
+    arb_log(logp, p, prec);
+    arb_add_si(r, a, 1, prec);
+    arb_lgamma(r, r, prec);
+    arb_add(r, r, logp, prec);
+    arb_div(r, r, a, prec);
+    arb_exp(r, r, prec);
+
+    arb_clear(logp);
+}
+
+static void
+_si_poly(arb_t res, const slong *coeffs, slong len, const arb_t x, slong prec)
+{
+    slong i;
+    arb_zero(res);
+    for (i = len-1; i > -1; i--)
+    {
+        arb_mul(res, res, x, prec);
+        arb_add_si(res, res, coeffs[i], prec);
+    }
+}
+
+static void
+_temme_3_3(arb_t res, const arb_t a_in, const arb_t r, slong prec)
+{
+    slong i;
+    arb_ptr u;
+    arb_struct *c = _arb_vec_init(6);
+    arb_struct *a = _arb_vec_init(5);
+    for (i = 0; i < 5; i++)
+        arb_add_si(a + i, a_in, i, prec);
+
+    /* c0 */
+    arb_zero(c + 0);
+
+    /* c1 */
+    arb_one(c + 1);
+
+    /* c2 */
+    u = c + 2;
+    arb_one(u);
+    arb_div(u, u, a + 1, prec);
+
+    /* c3 */
+    u = c + 3;
+    {
+        slong coeffs[] = {5, 3};
+        _si_poly(u, coeffs, 2, a, prec);
+    }
+    arb_div_si(u, u, 2, prec);
+    arb_div(u, u, a + 1, prec);
+    arb_div(u, u, a + 1, prec);
+    arb_div(u, u, a + 2, prec);
+
+    /* c4 */
+    u = c + 4;
+    {
+        slong coeffs[] = {31, 33, 8};
+        _si_poly(u, coeffs, 3, a, prec);
+    }
+    arb_div_si(u, u, 3, prec);
+    arb_div(u, u, a + 1, prec);
+    arb_div(u, u, a + 1, prec);
+    arb_div(u, u, a + 1, prec);
+    arb_div(u, u, a + 2, prec);
+    arb_div(u, u, a + 3, prec);
+
+    /* c5 */
+    u = c + 5;
+    {
+        slong coeffs[] = {2888, 5661, 3971, 1179, 125};
+        _si_poly(u, coeffs, 5, a, prec);
+    }
+    arb_div_si(u, u, 24, prec);
+    arb_div(u, u, a + 1, prec);
+    arb_div(u, u, a + 1, prec);
+    arb_div(u, u, a + 1, prec);
+    arb_div(u, u, a + 1, prec);
+    arb_div(u, u, a + 2, prec);
+    arb_div(u, u, a + 2, prec);
+    arb_div(u, u, a + 3, prec);
+    arb_div(u, u, a + 4, prec);
+
+    {
+        slong i;
+        arb_zero(res);
+        for (i = 6-1; i > -1; i--)
+        {
+            arb_mul(res, res, r, prec);
+            arb_add(res, res, c + i, prec);
+        }
+    }
+
+    _arb_vec_clear(a, 5);
+    _arb_vec_clear(c, 6);
+}
+
+/* return 0 if there is no error */
+static int
+_gamma_quantile_temme_3_3(arb_t res, const arb_t s, const arb_t p, slong prec)
+{
+    int result;
+    arb_t r, threshold;
+    arb_init(r);
+    arb_init(threshold);
+    _temme_r(r, s, p, prec);
+    arb_add_si(threshold, s, 1, prec);
+    arb_div_si(threshold, threshold, 5, prec);
+    if (arb_lt(r, threshold))
+    {
+        _temme_3_3(res, s, r, prec);
+        /*
+         * Multiply by an irrational number
+         * so that we can determine which side of the root we are on.
+         */
+        {
+            arb_t sqrt2;
+            arb_init(sqrt2);
+            arb_sqrt_ui(sqrt2, 2, prec);
+            arb_mul(res, res, sqrt2, prec);
+            arb_clear(sqrt2);
+        }
+
+        if (GAMMA_DISC_DEBUG)
+        {
+            flint_printf("debug (Eq. 3.3):\n");
+            flint_printf("prec=%wd\n", prec);
+            flint_printf("s:\n");
+            arb_printd(s, 15); flint_printf("\n");
+            flint_printf("p:\n");
+            arb_printd(p, 15); flint_printf("\n");
+            flint_printf("initial (temme Eq. 3.3 times sqrt 2):\n");
+            arb_printd(res, 15); flint_printf("\n");
+        }
+
+        result = 0;
+    }
+    else
+    {
+        result = -1;
+    }
+    arb_clear(r);
+    arb_clear(threshold);
     return result;
 }
 
@@ -187,20 +379,53 @@ gamma_quantile(arb_t res, slong k, slong n, const arb_t s, slong prec)
     quantile_args_t p;
     arf_interval_t r, refined;
     slong maxiter;
+    arf_t initial;
+    arb_t guess;
 
     quantile_args_init(p);
     arf_interval_init(r);
     arf_interval_init(refined);
+    arf_init(initial);
+    arb_init(guess);
 
     arb_set(p->s, s);
     arb_set_si(p->c, k);
     arb_div_si(p->c, p->c, n, prec);
 
-    result = isolate_positive_quantile(r, p, prec);
+    /*
+     * Compute r.
+     * If r < (1 + s) / 5 then use (3.3) of
+     * "Efficient and accurate algorithms for the computation
+     * and inversion of the incomplete gamma function ratios"
+     * to initialize a Newton-Raphson root search.
+     * Otherwise use bisection.
+     */
+    result = _gamma_quantile_temme_3_3(guess, p->s, p->c, prec);
+    if (result == 0)
+    {
+        arf_set(initial, arb_midref(guess));
+    }
+    else
+    {
+        arf_one(initial);
+    }
+
+    result = isolate_positive_quantile(r, initial, p, prec);
     if (result != ARB_CALC_SUCCESS)
     {
+        if (GAMMA_DISC_DEBUG)
+        {
+            flint_printf("(debug): failed to isolate quantile\n");
+        }
         arb_indeterminate(res);
         goto finish;
+    }
+    else
+    {
+        if (GAMMA_DISC_DEBUG)
+        {
+            flint_printf("(debug): successfully isolated quantile\n");
+        }
     }
 
     maxiter = 10000;
@@ -208,11 +433,19 @@ gamma_quantile(arb_t res, slong k, slong n, const arb_t s, slong prec)
             refined, _arb_gamma_objective, p, r, maxiter, prec);
     arf_interval_get_arb(res, refined, prec);
 
+    if (GAMMA_DISC_DEBUG)
+    {
+        flint_printf("(debug): refined quantile:\n");
+        arb_printd(res, 15); flint_printf("\n");
+    }
+
 finish:
 
     quantile_args_clear(p);
     arf_interval_clear(r);
     arf_interval_clear(refined);
+    arf_clear(initial);
+    arb_clear(guess);
 }
 
 void
