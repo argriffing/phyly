c83e88771f59d7b6e31992d6fa815a31e05c1d88
alex <argriffi@ncsu.edu>
2016-06-08 19:29:09 -0400

    update an example

--- old-examples/phyl-log-likelihood-test.py
+++ old-examples/phyl-log-likelihood-test.py
@@ -3,6 +3,7 @@
 from __future__ import print_function, division
 
 from StringIO import StringIO
+from functools import partial
 import json
 import itertools
 import math
@@ -15,6 +16,7 @@ from scipy.special import gamma, gammainc, expit, logit
 
 from arbplf import arbplf_ll, arbplf_deriv
 
+_edges = [[4, 0], [4, 1], [5, 4], [5, 2], [5, 3]]
 _character_definitions = [
         [1, 0, 0, 0],
         [0, 1, 0, 0],
@@ -39,6 +41,20 @@ _character_data = [
         [2, 2, 2, 1, 4, 4],
         [3, 3, 3, 3, 4, 4],
         [1, 1, 2, 0, 4, 4]]
+
+def _finite_diffs(objective, request, D, X, y):
+    for i, b in enumerate(request):
+        if not b:
+            continue
+        U = X.copy()
+        if X[i]:
+            h = X[i] * 1e-8
+        else:
+            h = 1e-8
+        U[i] = X[i] + h
+        v = objective(U)
+        D[i] = (v - y) / h
+
 
 def T92(k, t=0.5):
     # try to use the Bio++ T92 parameterization
@@ -117,28 +133,54 @@ def discretized_gamma(N, alpha, use_mean=1):
     #print(rates)
     return rates
 
-def mixture_objective(X):
+def generic_neg_ll(get_s, X):
+    s_in = get_s(X)
+    s_out = arbplf_ll(s_in)
+    df = pd.read_json(StringIO(s_out), orient='split', precise_float=True)
+    y = -df.value.values[0]
+    print(y)
+    return y
+
+def generic_objective(get_s, X):
+    s_in = get_s(X)
+    neg_ll = partial(generic_neg_ll, get_s)
+
+    # compute negative log likelihood
+    s_out = arbplf_ll(s_in)
+    df = pd.read_json(StringIO(s_out), orient='split', precise_float=True)
+    y = -df.value.values[0]
+
+    # explicitly compute derivatives with respect to edge rate coefficients
+    s_out = arbplf_deriv(s_in)
+    df = pd.read_json(StringIO(s_out), orient='split', precise_float=True)
+    d = -df.set_index('edge').value.values
+
+    edge_count = len(_edges)
+    D = np.concatenate((d, [0]*3))
+    request = np.array([0]*edge_count + [1]*3)
+    _finite_diffs(neg_ll, request, D, X, y)
+    print(y, D)
+    return y, D
+
+def mixture_get_s(X):
     # same as block_objective, but implemented with a rate mixture
     #print(X)
     kappa, logit_theta, alpha = X[-3:]
     edge_rates = X[:-3].tolist()
 
-    print('alpha:', alpha)
+    #print('alpha:', alpha)
 
     theta = expit(logit_theta)
     t = theta
     pi = [(1-t)/2, t/2, t/2, (1-t)/2]
-    print('pi:', pi)
+    #print('pi:', pi)
 
     #alpha = 1.0
 
     rate_category_count = 4
     state_count = 4
-    edge_count = 5
+    edge_count = len(_edges)
     node_count = edge_count + 1
-
-    # Define the tree used in the phyl transition mapping example.
-    edges = [[4, 0], [4, 1], [5, 4], [5, 2], [5, 3]]
 
     # Use a T92 rate matrix.
     R, x = T92(kappa, theta)
@@ -154,12 +196,12 @@ def mixture_objective(X):
             prior = 'uniform_distribution')
     gamma_rate_mixture = dict(gamma_shape=alpha, gamma_categories=4)
     model_and_data = {
-            "edges" : edges,
+            "edges" : _edges,
             "rate_divisor" : 100 * x,
             'gamma_rate_mixture' : gamma_rate_mixture,
             "edge_rate_coefficients" : edge_rates,
             "rate_matrix" : rate_matrix,
-            'root_prior' : 'uniform_distribution',
+            'root_prior' : pi,
             'character_definitions' : _character_definitions,
             'character_data' : _character_data,
             }
@@ -167,19 +209,11 @@ def mixture_objective(X):
             "model_and_data" : model_and_data,
             "site_reduction" : {"aggregation" : "sum"}}
     s = json.dumps(d)
-    try:
-        s = arbplf_ll(s)
-    except RuntimeError as e:
-        return np.inf
-    df = pd.read_json(StringIO(s), orient='split', precise_float=True)
-    y = -df.value.values[0]
-    #print(y)
-    return y
+    return s
 
 
-def block_objective(X):
+def block_get_s(X):
     # implemented with a block matrix
-    #print(X)
     kappa, logit_theta, alpha = X[-3:]
     edge_rates = X[:-3].tolist()
 
@@ -187,63 +221,54 @@ def block_objective(X):
     t = theta
     pi = [(1-t)/2, t/2, t/2, (1-t)/2]
 
-    #alpha = 1.0
-
     rate_category_count = 4
     state_count = 4
-    edge_count = 5
+    edge_count = len(_edges)
     node_count = edge_count + 1
-
-    # Define the tree used in the phyl transition mapping example.
-    edges = [[4, 0], [4, 1], [5, 4], [5, 2], [5, 3]]
 
     # Use a T92 rate matrix.
     R, x = T92(kappa, theta)
     mixture_rates = discretized_gamma(rate_category_count, alpha)
     rate_matrix = block_rate_mixture(R, mixture_rates)
 
-
     # Sequences at internal nodes consist of unobserved nucleotide N.
     # The gamma rate class is unobserved.
+    defs = [lst*4 for lst in _character_definitions]
+    pi = [0.25 * x for x in pi*4]
 
     model_and_data = {
-            "edges" : edges,
+            "edges" : _edges,
             "rate_divisor" : 100 * x,
             "edge_rate_coefficients" : edge_rates,
             "rate_matrix" : rate_matrix,
-            'root_prior' : 'uniform_distribution',
-            'character_definitions' : _character_definitions,
+            'root_prior' : pi,
+            'character_definitions' : defs,
             'character_data' : _character_data,
             }
     d = {
             "model_and_data" : model_and_data,
             "site_reduction" : {"aggregation" : "sum"}}
     s = json.dumps(d)
-    try:
-        s = arbplf_ll(s)
-    except RuntimeError as e:
-        return np.inf
-    df = pd.read_json(StringIO(s), orient='split', precise_float=True)
-    y = -df.value.values[0]
-    #print(y)
-    return y
+    return s
 
 
 def main():
-    objective = mixture_objective
-    #objective = block_objective
+    get_s = block_get_s
+    #get_s = mixture_get_s
 
-    #edge_rates = [1, 30, 1, 30, 30]
-    #kappa = 0.2
-    #theta = 0.5
-    #alpha = 2.0
-    edge_rates = [1, 2, 3, 1, 10]
-    alpha = 1.0
-    kappa = 3.0
+    objective = partial(generic_objective, get_s)
+    neg_ll = partial(generic_neg_ll, get_s)
+
+    edge_rates = [1, 30, 1, 30, 30]
+    kappa = 0.2
     theta = 0.5
+    alpha = 2.0
+    #edge_rates = [1, 2, 3, 1, 10]
+    #alpha = 1.0
+    #kappa = 3.0
+    #theta = 0.5
     X0 = np.array(edge_rates + [kappa, logit(theta), alpha], dtype=float)
-    print('%.20g' % objective(X0))
-    return
+    print('%.20g' % neg_ll(X0))
 
     desired_ll = 85.030942031997312824
     #edge_rates = [1, 2, 3, 1, 10]
@@ -252,7 +277,9 @@ def main():
     a = 1e-6
     bounds = [(a, None) for i in X0[:5]] + [(a, None), (a, 1), (a, None)]
     result = optimize.minimize(
-            objective, X0, method='L-BFGS-B', bounds=bounds)
+            objective, X0, method='L-BFGS-B', jac=True, bounds=bounds)
+    #result = optimize.minimize(
+            #neg_ll, X0, method='L-BFGS-B', bounds=bounds)
     print(result)
 
 main()
